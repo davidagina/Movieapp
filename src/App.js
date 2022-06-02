902f3cb67639433cc295d7530888cfdac1a1720a
davidagina <davidagina64@gmail.com>
2022-06-02 09:15:44 +0100

    movieapp

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useEffect} from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+
+const movie1 = {
+        "Title": "Superman, Spiderman or Batman",
+        "Year": "2011",
+        "imdbID": "tt2084949",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+    }
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const API_URL = 'http://www.omdbapi.com/?apikey=bc4e7d08'
+    
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+    
+        setMovies(data.Search)
+    }
+    
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, [])
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+                <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)}/>
+            </div>
+
+           {
+               movies?.length > 0 ?
+               (
+                   <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>
+               ) : 
+               (
+                   <div className="empty">
+                       <h2>No movies found</h2>
+                   </div>
+               )
+           }
+            
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+// Here is your key: bc4e7d08
+
+// Please append it to all of your API requests,
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=bc4e7d08
